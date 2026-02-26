1142a9a3b30af06503a1b73cea45767966712987
Alberto J Garcia <albertojg1991@gmail.com>
2026-02-25 22:07:50 -0800

    Moved to RAGChat class found in rag_chat.py.

--- src/rag_chatbot/rag_prompt.py
+++
@@ -1,9 +0,0 @@
-def build_rag_prompt(system_prompt, retrieved_docs, user_question):
-    context = "\n".join(retrieved_docs)
-    return (
-        f"{system_prompt}\n\n"
-        f"Use ONLY the information below to answer.\n"
-        f"Context:\n{context}\n\n"
-        f"Question: {user_question}\n"
-        f"Answer:"
-    )
