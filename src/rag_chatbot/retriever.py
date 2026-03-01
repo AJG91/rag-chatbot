7676b5eb1bbaffc98a8881678249189a0accd484
Alberto J Garcia <albertojg1991@gmail.com>
2026-03-01 13:20:52 -0800

    Added docstring.

--- src/rag_chatbot/retriever.py
+++ src/rag_chatbot/retriever.py
@@ -2,7 +2,23 @@ import faiss
 from sentence_transformers import SentenceTransformer
 
 class DocsRetriever:
+    """
+    A class used to retrieve information from documents similar to a query.
+    
+    Attributes
+    ----------
+    documents : list[str]
+    model : str
+    index : faiss.Index
+        Constructs an exact FAISS index using squared L2 distance.
 
+    Parameters
+    ----------
+    documents : list[str]
+        List of text that will be encoded and retrieved.
+    model : str, optional (default="all-MiniLM-L6-v2")
+        Name of model that will be loaded.
+    """
     def __init__(
         self, 
         documents: list[str], 
@@ -20,6 +36,21 @@ class DocsRetriever:
         query: str, 
         k: int = 1
     ) -> list[str]:
+        """
+        Retrieves the top k most similar documents in the query string.
+
+        Parameters
+        ----------
+        query : str
+            Natural language query to embed and search against the index.
+        k : int, optional (default=1)
+            Number of nearest documents to return. Must be >= 1.
+
+        Returns
+        -------
+        list[str]
+            List of retrieved documents ordered by increasing L2 distance         (i.e., most similar first).
+        """
         query_emb = self.model.encode([query], convert_to_numpy=True)
         _, idcs = self.index.search(query_emb, k)
         return [self.documents[i] for i in idcs[0]]
